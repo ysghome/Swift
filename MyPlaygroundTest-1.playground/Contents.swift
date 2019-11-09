f0ce64765cae27d2741e73712253c1a5d2b8109f
yinshaogang <yinshaogang@founq.com>
2019-11-09 14:17:13 +0800

    Demo练习添加

---
+++ MyPlaygroundTest-1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str = "Hello, playground"
+//数据类型都是 大写开头
+var a:Int = 10
+
+var b:String = "Hello world"
+
+var c:Character = "a"
+
+var d:Double = 1.2222
+
+var e:Bool = true
+
+let f:Int = 20
+
+//print(type(of: str))
+
+print("sdlkfjlaj\nsdjflajsdlf")
+//字符串 拼接
+print("\(a) " + b + "\(e)")
---
+++ MyPlaygroundTest-2.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+//var str = "Hello, playground"
+
+//typealias dog = Int
+//
+//var a:dog = 10
+//
+//var b:Int = 20
+//
+//print(a,b)
+//
+//typealias s = String
+//
+//let c:s = "sdfjlj"
+//
+//let d:String = "dsfsd"
+//
+//print(c + d)
+//
+//
+//var a = 10
+//
+//print("值等于 = " + String(a))
+//
+//var a = "10"
+//
+//print(Int(a) ?? 0)
+
+//var a:Int? = 10
+////可选类型 可能有值，可能没值
+//var b:Int? = nil
+//
+////print(a,b)
+//
+//print(a ?? 0)
+//print(a!)//知道有值的时候，使用变量明!
+//print(b ?? 100)
+
+var value:String = "hello world"
+
+var a:Int? = 10
+
+if (a == nil) {
+    print("没有值")
+} else{
+    print("a的值 = \(a!)")
+}
+
+
