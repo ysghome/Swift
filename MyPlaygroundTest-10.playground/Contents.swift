12097b632874b4dd17fa265a6cdd80af81fd1bae
yinshaogang <yinshaogang@founq.com>
2020-05-29 14:15:32 +0800

    【update】add

--- MyPlaygroundTest-10.playground/Contents.swift
+++ MyPlaygroundTest-10.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var str = "Hello, playground"
+var str = "ABCDEFG"
+
+//print(str.startIndex)
+//
+//print(str.endIndex)
+
+print(str[str.startIndex])
+
+print(str[str.index(before: str.endIndex)])
